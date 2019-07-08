e6d8b4568da4d545ec7c7ea92c4e2e950dd52438
Chad Garrett <cgarrett@retrorabbit.co.za>
2019-07-08 13:57:33 +0200

    srn updated gitignore

--- Pods/Target Support Files/CoreAardvark/CoreAardvark-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CoreAardvark : NSObject
-@end
-@implementation PodsDummy_CoreAardvark
-@end
--- Pods/Target Support Files/Pods-ChaiTests/Pods-ChaiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChaiTests : NSObject
-@end
-@implementation PodsDummy_Pods_ChaiTests
-@end
