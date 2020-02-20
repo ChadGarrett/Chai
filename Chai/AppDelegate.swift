5e03856aff6516851706bd84f60a245f4361fda6
Chad Garrett <cgarrett@retrorabbit.co.za>
2020-02-20 17:32:52 +0200

    Show notification banners for network status changes

--- Chai/AppDelegate.swift
+++ Chai/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         MigrationService(),
         LoggingService(),
         CrashService(),
+        BannerService(),
     ]
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- Chai/Services/BannerService.swift
+++ Chai/Services/BannerService.swift
@@ -6,21 +6,37 @@
 //  Copyright © 2019 Chad Garrett. All rights reserved.
 //
 
+import Network
 import NotificationBannerSwift
 
 /// Displays a banner at the top of the screen informing the user of an action or error
 final class BannerService {
     static let shared = BannerService()
     
-    internal func showStatusBarBanner(title: String, style: BannerStyle) {
+    enum Duration: TimeInterval {
+        /// 1 second
+        case short = 1.0
+        
+        /// 4 seconds
+        case medium = 4
+        
+        /// 8 seconds
+        case long = 8
+    }
+    
+    internal func showStatusBarBanner(title: String, style: BannerStyle, duration: Duration = .medium, autoDismiss: Bool = true) {
         let banner = StatusBarNotificationBanner(title: title, style: style)
         banner.dismissOnSwipeUp = true
+        banner.autoDismiss = autoDismiss
+        banner.duration = duration.rawValue
         banner.show()
     }
     
-    internal func showBanner(title: String, subtitle: String? = nil, style: BannerStyle) {
+    internal func showBanner(title: String, subtitle: String? = nil, style: BannerStyle, duration: Duration = .medium, autoDismiss: Bool = true) {
         let banner = NotificationBanner(title: title, subtitle: subtitle, style: style)
         banner.dismissOnSwipeUp = true
+        banner.autoDismiss = autoDismiss
+        banner.duration = duration.rawValue
         banner.show()
     }
     
@@ -28,5 +44,34 @@ final class BannerService {
     internal func showNotImplementedBanner() {
         let banner = NotificationBanner(title: "Error :(", subtitle: "Feature not implemented!", style: .danger)
         banner.show()
+    }
+}
+
+extension BannerService: BaseService {
+    func setup() {
+        NetworkStatusMonitor.shared.attachObserver(self)
+    }
+    
+    func teardown() {
+        NetworkStatusMonitor.shared.removeObserver(self)
+    }
+}
+
+extension BannerService: NetworkStatusObserver {
+    var identifier: String {
+        return "BannerService"
+    }
+    
+    func networkStatusDidUpdate(to status: NWPath.Status) {
+        DispatchQueue.main.async { [weak self] in
+            switch status {
+            case .satisfied:
+                self?.showStatusBarBanner(title: "Connected!", style: .success, duration: .short)
+            case .unsatisfied:
+                self?.showStatusBarBanner(title: "No connection available.", style: .danger, duration: .long)
+            case .requiresConnection:
+                self?.showBanner(title: "Unknown connection", style: .danger)
+            }
+        }
     }
 }
